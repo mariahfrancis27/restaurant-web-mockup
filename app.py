860ef2b93139c40663ca10f89f707fadcd9fc958
mariahfrancis27 <mariahfrancis27@gmail.com>
2026-07-13 13:56:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -776,4 +776,8 @@ def run_prediction(n_clicks, store, dow, hour, payment, n_items, total_spent, n_
 # ============================================================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8050)))
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8050)),
+        debug=False
+    )
